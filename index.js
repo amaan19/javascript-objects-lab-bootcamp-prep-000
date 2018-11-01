5f1b2aa50f88ce7e1466c13d5eb17fac977a5eb3
Amaan Ahmed <amaan.ahmed3@gmail.com>
2018-11-01 16:58:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-
+recipes = {};
+function updateObjectWithKeyAndValue (obj,key,value) {
+  return Object.assign({}, {[key]: value});
+}
+function destructivelyupdateObjectWithKeyAndValue (obj, key, value) {
+  return Object.assign({[key]: value});
+}
+function deleteFromObjectByKey (obj,key) {
+  delete obj[key];
+  return Object.assign({}, obj);
+}
+function destructivelyDeleteFromObjectByKey (obj, key) {
+  delete obj[key];
+  return obj
+}
